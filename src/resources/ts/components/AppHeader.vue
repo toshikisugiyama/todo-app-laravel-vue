83d146e5d029d8166fffaab0556ce41c5c7a534e
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-12-03 00:10:49 +0900

    implement user logout

---
+++ src/resources/ts/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  header.container-fluid.bg-primary.py-1.px-auto.px-sm-5
+    .row.row-cols-2.align-items-center
+      .col
+        h1.m-0.text-light ToDo App
+      .col.d-flex.justify-content-end
+        .col-auto.p-0.rounded.bg-light(v-if="currentUser")
+          button.btn.btn-outline-primary.btn-sm(@click.prevent="logout") ログアウト
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  computed: {
+    currentUser () {
+      return this.$store.state.auth.user
+    }
+  },
+  methods: {
+    async logout () {
+      await this.$store.dispatch('auth/logout')
+      if (!this.currentUser) {
+        this.$router.push('/login')
+      }
+    }
+  }
+})
+</script>
